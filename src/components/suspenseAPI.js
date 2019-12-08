c1a5e59f4e335afca8a6999aea6b4fe10fd9fb7a
balkanskiy <balkanskiy.alexey@gmail.com>
2019-12-08 16:48:11 +0300

    Add Suspense API method

---
+++ src/components/suspenseAPI.js
@@ -0,0 +1,31 @@
+// Suspense integrations like Relay implement
+// a contract like this to integrate with React.
+// Real implementations can be significantly more complex.
+// Don't copy-paste this into your project!
+function wrapPromise(promise) {
+  let status = "pending";
+  let result;
+  let suspender = promise.then(
+    r => {
+      status = "success";
+      result = r;
+    },
+    e => {
+      status = "error";
+      result = e;
+    }
+  );
+  return {
+    read() {
+      if (status === "pending") {
+        throw suspender;
+      } else if (status === "error") {
+        throw result;
+      } else if (status === "success") {
+        return result;
+      }
+    }
+  };
+}
+
+console.log(wrapPromise);
